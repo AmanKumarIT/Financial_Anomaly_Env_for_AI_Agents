a755d31088daf837bd096a06a247cce55a354a2a
AmanKumarIT <akpailwar120@gmail.com>
2026-04-08 19:33:17 +0530

    Finalized

--- server/app.py
+++ server/app.py
@@ -4,3 +4,12 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from server import app
+import uvicorn
+
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
